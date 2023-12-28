09346f103015299e2c21a3b98382afcbc8abecf1
Morris, Kevin <Kevin.Morris@noblis.org>
2023-12-28 08:14:39 -0500

    Initialize spring boot application

---
+++ src/main/java/com/vino/vinoapi/VinoApiApplication.java
@@ -0,0 +1,13 @@
+package com.vino.vinoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vino/vinoapi/VinoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vino.vinoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
